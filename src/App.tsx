85c7350d21f6be7cb531030d2a835c9163515cc8
Sami Mustafa <56640056+KelpieAI@users.noreply.github.com>
2026-03-25 23:56:43 +0000

    Update routing for persistent AppShell layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,31 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate, Outlet } from 'react-router-dom';
 import { LoginPage } from './pages/LoginPage';
 import { ProtectedRoute } from './components/ProtectedRoute';
 import { NessieQueue } from './pages/NessieQueue';
 import { CreateBatchPage } from './pages/CreateBatchPage';
 import { SettingsPage } from './pages/SettingsPage';
-import { NessieStatusBar } from './components/NessieStatusBar';
-// ... other imports
+import { AppShell } from './components/nessie/AppShell';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Public route */}
         <Route path="/login" element={<LoginPage />} />
-        
-        {/* Protected routes */}
-        <Route
-          path="/queue"
-          element={
-            <ProtectedRoute>
-              <NessieQueue />
-            </ProtectedRoute>
-          }
-        />
 
         <Route
-          path="/queue/new"
           element={
             <ProtectedRoute>
-              <CreateBatchPage />
+              <AppShell />
             </ProtectedRoute>
           }
-        />
+        >
+          <Route path="/queue" element={<NessieQueue />} />
+          <Route path="/queue/new" element={<CreateBatchPage />} />
+          <Route path="/settings" element={<SettingsPage />} />
+        </Route>
 
-        <Route
-          path="/settings"
-          element={
-            <ProtectedRoute>
-              <SettingsPage />
-            </ProtectedRoute>
-          }
-        />
-        
-        {/* Redirect root to queue */}
         <Route path="/" element={<Navigate to="/queue" replace />} />
       </Routes>
-      
-      {/* Persistent footer – shown on ALL pages */}
-      <NessieStatusBar />
     </BrowserRouter>
   );
 }
